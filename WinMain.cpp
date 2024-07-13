c2992d6140d7cd4bf37594aae778421f12e07272
evvic <ewic98@gmail.com>
2024-07-13 16:36:49 -0700

    create window class and initiate it

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,6 +6,42 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow
 ) {
+	const WCHAR* pClassName = L"hw3d";
+
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof( wc );
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx( &wc );
+
+	// create window instance
+
+	HWND hWnd = CreateWindowEx(
+		0,	
+		pClassName,
+		L"Cool window",	
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	// show the window after creating it
+	ShowWindow( hWnd, SW_SHOW );
+
 	while (true);
 	return 0;
 }
