15fe902158e1adbb692f2c2037f7cf7e259a5570
evvic <ewic98@gmail.com>
2024-07-13 17:01:19 -0700

    handle windows messages with custom winodws procedure

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+
+// Window procedure, handles messages from Windows
+// Wrapper around DefWindowProc (for additional handling)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -12,7 +24,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -42,6 +54,22 @@ int CALLBACK WinMain(
 	// show the window after creating it
 	ShowWindow( hWnd, SW_SHOW );
 
-	while (true);
+	// process messages from Windows
+	MSG msg;
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	// handle exit code
+	if (gResult == -1) {
+		return -1;
+	}
+	else {
+		return msg.wParam;
+	}
+
 	return 0;
 }
