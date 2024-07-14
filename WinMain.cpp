f61044e12662ebca86631debf047fc108699dbdc
evvic <ewic98@gmail.com>
2024-07-13 22:06:27 -0700

    move Window definition and instantiation into a framework

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,51 +1,5 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "Window.h"
 
-
-// Window procedure, handles messages from Windows
-// Wrapper around DefWindowProc (for additional handling)
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Respects");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Disrespects");
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::string title;
-		title.push_back((char)wParam);
-		SetWindowText(hWnd, title.c_str());
-	}
-	break;
-	case WM_LBUTTONDOWN:
-	{
-		const POINTS pt = MAKEPOINTS(lParam);
-		std::ostringstream oss;
-		oss << "(" << pt.x << "," << pt.y << ")";
-		SetWindowText(hWnd, oss.str().c_str());
-	}
-	break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -53,41 +7,9 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow
 ) {
-	const auto pClassName = "hw3d";
-
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof( wc );
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx( &wc );
 
 	// create window instance
-
-	HWND hWnd = CreateWindowEx(
-		0,	
-		pClassName,
-		"Win32 window",	
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr,
-		nullptr,
-		hInstance,
-		nullptr
-	);
-
-	// show the window after creating it
-	ShowWindow( hWnd, SW_SHOW );
+	Window wnd(800, 600, "Direct3D Engine Window");
 
 	// process messages from Windows
 	MSG msg;
@@ -102,9 +24,6 @@ int CALLBACK WinMain(
 	if (gResult == -1) {
 		return -1;
 	}
-	else {
-		return msg.wParam;
-	}
 
-	return 0;
+	return (int)msg.wParam;
 }
